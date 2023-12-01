6a2247f25b916bbc44cefb9f0ad3160cb4e15752
CrisL04 <152100526+CrisL04@users.noreply.github.com>
2023-11-30 23:32:38 -0400

    Program.cs

--- citas/citas/citas/Program.cs
+++ citas/citas/citas/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace citas
 {
@@ -41,7 +39,11 @@ namespace citas
 
             static void MostrarMenu()
             {
+                Console.WriteLine();
+                Console.WriteLine("****************************** Sistema de citas para peluquería ******************************");
+                Console.WriteLine();
                 Console.WriteLine("Menú:");
+                Console.WriteLine();
                 Console.WriteLine("1. Agregar Cliente");
                 Console.WriteLine("2. Programar Cita");
                 Console.WriteLine("3. Mostrar Citas");
@@ -51,6 +53,7 @@ namespace citas
 
             static int ObtenerOpcionUsuario()
             {
+                Console.WriteLine();
                 Console.Write("Seleccione una opción: ");
                 return Convert.ToInt32(Console.ReadLine());
             }
@@ -82,11 +85,17 @@ namespace citas
 
             static void AgregarCliente()
             {
+                Console.Clear();
                 Console.WriteLine("Ingrese el nombre del cliente:");
-                string nombre = Console.ReadLine();
+                string nombre = SafeReadLine();
 
-                Console.WriteLine("Ingrese el número de teléfono del cliente:");
-                string telefono = Console.ReadLine();
+                // Validación para el número de teléfono
+                string telefono;
+                do
+                {
+                    Console.WriteLine("Ingrese el número de teléfono del cliente (solo números y debe tener 10 dígitos):");
+                    telefono = SafeReadLine();
+                } while (!EsNumero(telefono) || telefono.Length != 10);
 
                 Cliente cliente = new Cliente
                 {
@@ -97,6 +106,13 @@ namespace citas
 
                 clientes.Add(cliente);
                 Console.WriteLine($"Cliente {cliente.Nombre} agregado con éxito.");
+                Console.Clear();
+            }
+
+            // Método para verificar si una cadena es un número
+            static bool EsNumero(string input)
+            {
+                return input.All(char.IsDigit);
             }
 
             static void ProgramarCita()
@@ -107,21 +123,23 @@ namespace citas
                     return;
                 }
 
+                Console.Clear();
                 Console.WriteLine("Seleccione el cliente para la cita:");
                 MostrarClientes();
 
-                int idCliente = Convert.ToInt32(Console.ReadLine());
+                int idCliente = Convert.ToInt32(SafeReadLine());
                 Cliente cliente = ObtenerClientePorId(idCliente);
 
+                Console.Clear();
                 Console.WriteLine("Ingrese la fecha y hora de la cita (yyyy-MM-dd HH:mm):");
                 DateTime fechaHora;
-                while (!DateTime.TryParseExact(Console.ReadLine(), "yyyy-MM-dd HH:mm", null, System.Globalization.DateTimeStyles.None, out fechaHora))
+                while (!DateTime.TryParseExact(SafeReadLine(), "yyyy-MM-dd HH:mm", null, System.Globalization.DateTimeStyles.None, out fechaHora))
                 {
                     Console.WriteLine("Formato de fecha y hora incorrecto. Intente nuevamente.");
                 }
 
                 Console.WriteLine("Ingrese el servicio de la cita:");
-                string servicio = Console.ReadLine();
+                string servicio = SafeReadLine();
 
                 Cita cita = new Cita
                 {
@@ -130,13 +148,14 @@ namespace citas
                     Cliente = cliente,
                     Servicio = servicio
                 };
-
+                Console.Clear();
                 citas.Add(cita);
                 Console.WriteLine($"Cita programada con éxito para {cliente.Nombre} el {fechaHora} para el servicio de {servicio}.");
             }
 
             static void MostrarClientes()
             {
+                Console.Clear();
                 Console.WriteLine("Lista de Clientes:");
                 foreach (var cliente in clientes)
                 {
@@ -146,6 +165,7 @@ namespace citas
 
             static void MostrarCitas()
             {
+                Console.Clear();
                 if (citas.Count == 0)
                 {
                     Console.WriteLine("No hay citas programadas actualmente.");
@@ -161,6 +181,7 @@ namespace citas
 
             static void CancelarCita()
             {
+                Console.Clear();
                 if (citas.Count == 0)
                 {
                     Console.WriteLine("No hay citas programadas para cancelar.");
@@ -169,7 +190,7 @@ namespace citas
 
                 Console.WriteLine("Ingrese el ID de la cita que desea cancelar:");
                 int idCita;
-                while (!int.TryParse(Console.ReadLine(), out idCita))
+                while (!int.TryParse(SafeReadLine(), out idCita))
                 {
                     Console.WriteLine("Ingrese un número válido.");
                 }
@@ -185,6 +206,7 @@ namespace citas
                 {
                     Console.WriteLine("No se encontró ninguna cita con el ID proporcionado.");
                 }
+                Console.Clear();
             }
 
             static Cliente ObtenerClientePorId(int id)
@@ -202,7 +224,39 @@ namespace citas
                 Console.WriteLine("Saliendo del programa. ¡Hasta luego!");
                 Environment.Exit(0);
             }
+
+            // Bloquea la tecla Enter si no se ha ingresado nada
+            static string SafeReadLine()
+            {
+                string input = "";
+                while (true)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        if (!string.IsNullOrWhiteSpace(input))
+                        {
+                            Console.WriteLine(); // Imprime una nueva línea para mantener el formato
+                            return input;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Por favor, ingrese un valor.");
+                        }
+                    }
+                    else if (key.Key == ConsoleKey.Backspace && input.Length > 0)
+                    {
+                        input = input.Substring(0, input.Length - 1);
+                        Console.Write("\b \b"); // Borra el carácter y mueve el cursor hacia atrás
+                    }
+                    else if (!char.IsControl(key.KeyChar))
+                    {
+                        input += key.KeyChar;
+                        Console.Write(key.KeyChar);
+                    }
+                }
+            }
         }
     }
 }
-        
